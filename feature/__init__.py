3ee766ca1ced75a77c738a690c046ab856ac18c5
jiadong <ted930511@gmail.com>
2021-11-27 20:41:38 +0800

    remove functions that not being used

--- feature/__init__.py
+++ feature/__init__.py
@@ -1,11 +1,6 @@
 from .feature_extraction import extract_patches
 from .feature_extraction import reconstruct_patches
-#from .feature_extraction import extract_daisy
-#from .feature_extraction import extract_hog
 
-#from .key_points import KeyPoints
-#from .key_point_v3 import KeyPoints
-#from .feature_points import FeaturePoints
 
 from .dim_reduction import decomposition_fa
 from .dim_reduction import decomposition_ica
@@ -16,25 +11,17 @@ from .dim_reduction import kpca
 from .dim_reduction import nmf
 from .dim_reduction import plot_pca
 
-#from .auto_clustering import kmeans_lbs
-#from .auto_clustering import gmm_lbs
-#from .auto_clustering import seg_lbs
-#from .auto_clustering import estimate_k
+#from .zps_matrix4 import zp_j2nm
+#from .zps_matrix4 import zp_nm2j
+#from .zps_matrix4 import zp_j2nm_complex
+#from .zps_matrix4 import zp_nm2j_complex
 
-#from .zps import ZPs
-#from .zps import zp_j2nm
-#from .zps import zp_nm2j
-#from .gpzp import GPZPs
-from .zps_matrix4 import zp_j2nm
-from .zps_matrix4 import zp_nm2j
-from .zps_matrix4 import zp_j2nm_complex
-from .zps_matrix4 import zp_nm2j_complex
-#from .zps_matrix4 import ZPs
+from .share_utils import zp_j2nm
+from .share_utils import zp_nm2j
+
 from .zps_matrix6 import ZPs
-#from .zmarray import zmarray
 from .bessel_matrix import Bessel
 
-#from .gpzp_matrix import GPZPs
 
 from .transform import gaussians
 from .transform import register_imgs
@@ -62,8 +49,6 @@ __all__ = [
            'register_imgs',
            'zp_j2nm',
            'zp_nm2j',
-           'zp_j2nm_complex',
-           'zp_nm2j_complex',
            'zmarray',
            'ptsarray',
            'Bessel',
