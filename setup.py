4ab897af868fbca87fc69cd686f6ae846347a55d
jiadongdan <ted930511@gmail.com>
2024-09-20 19:36:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
   author = 'Jiadong Dan',                   # Type in your name
   author_email = '',      # Type in your E-Mail
   url = 'https://github.com/jiadongdan/motif-learn',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['feature dimension reduction', 'manifold learning', 'Zernike Polynomials'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -16,6 +15,7 @@ setup(
           'numba',
           'scikit-image',
           'scikit-learn',
+          'matplotlib',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
